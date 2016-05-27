f5bdbd066bdbf866d3c6a591c5c7e7c2967bdb54
Gabe Rodriguez <grod220@gmail.com>
2016-05-27 15:20:10 -0400

    one more

---
+++ program.js
@@ -0,0 +1,44 @@
+// Hello World
+// console.log('HELLO WORLD');
+
+// Baby Steps
+// function nodeAdder (array) {
+//   var total = 0;
+//   for (var i = 2; i < array.length; i++) {
+//     total += +array[i];
+//   }
+//   return total;
+// }
+// console.log(nodeAdder(process.argv));
+
+// My first i/o
+// var fs = require('fs');
+// console.log(fs.readFileSync(process.argv[2]).toString().split('\n').length - 1);
+
+// My first async i/o
+// var fs = require('fs');
+// fs.readFile(process.argv[2], function (err, data) {
+//   console.log(data.toString().split('\n').length - 1);
+//   }
+// );
+
+// Filtered LS
+// console.log(process.argv);
+// var fs = require('fs');
+// var path = require('path');
+
+// fs.readdir(process.argv[2], function (err, data) {
+//       data.forEach(function(element) {
+//       if (path.extname(element) === '.' + process.argv[3]) {
+//           console.log(element);
+//       }
+//     });
+//   }
+// );
+      
+// Make it modular
+
+      
+      
+      
+      
